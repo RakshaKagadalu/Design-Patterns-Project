188e7d7e65456b10478ea7045dae1b6934a849e3
Raksha Kagadalu <98128238+Raksha-Kagadalu@users.noreply.github.com>
2023-04-26 19:40:42 -0400

    Changing gitignore.

--- src/main/java/edu/neu/csye7374/Driver.java
+++ src/main/java/edu/neu/csye7374/Driver.java
@@ -2,7 +2,7 @@ package edu.neu.csye7374;
 
 public class Driver {
     public static void main(String args[]) {
-        System.out.println("Initial commit");
+        System.out.println("NEUBikes");
         BikeDock.demo();
     }
 }
